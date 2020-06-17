250d28c26c1111640997d05e58c4b31808ec7f45
AlanLun <alan@bincentive.com>
2020-06-17 18:05:04 +0800

    add Facade

---
+++ DesignPatternStudy/Facade/MyJsonConverter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Facade
+{
+    public class MyJsonConverter
+    {
+        public static T Deserialize<T>(string json)
+        {
+            return System.Text.Json.JsonSerializer.Deserialize<T>(json);
+        }
+
+        public static string Serialize<T>(T source)
+        {
+            return System.Text.Json.JsonSerializer.Serialize<T>(source);
+        }
+    }
+}
---
+++ DesignPatternStudy/Facade/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Facade
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var testData = new Test()
+            {
+                Title = "123",
+                Content = "data"
+            };
+            var json = MyJsonConverter.Serialize<Test>(testData);
+            Console.WriteLine("Object to Json");
+            Console.WriteLine(json);
+            var testModel = MyJsonConverter.Deserialize<Test>(json);
+            Console.WriteLine("Json to Object");
+            Console.WriteLine(testModel.Content + testModel.Title);
+            Console.ReadLine();
+
+        }
+    }
+
+    public class Test
+    {
+        public string Title { get; set; }
+        public string Content { get; set; }
+
+    }
+}
